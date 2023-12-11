d9d18d422f53275273f761551332e965c96385fa
JanGaud <JanisGaudreault.jg@gmail.com>
2023-12-11 16:53:57 -0500

    Link for facebook added

--- src/lib/components/Socials.svelte
+++ src/lib/components/Socials.svelte
@@ -19,8 +19,8 @@
   }
 </style>
 
-<div class="mt-40 mb-20 h-20 w-full border border-main-white rounded-md flex items-center justify-around text-main-white">
-  <a href="/" class="glow-icon">
+<div class="mt-40 mb-20 h-20 w-full border-2 border-main-white rounded-md flex items-center justify-around text-main-white shadow-inner">
+  <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/100genies" class="glow-icon">
     <Icon class="w-14 h-14 hover:text-gold transition-colors" icon="ic:baseline-facebook" />
   </a>
   <a href="/" class="glow-icon">
