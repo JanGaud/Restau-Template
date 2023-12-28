eded6d23f171663b7555f838ce6b76a01128b51b
JanGaud <JanisGaudreault.jg@gmail.com>
2023-12-28 12:45:58 -0500

    optimised and blurred navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -10,18 +10,26 @@
 			document.body.classList.remove('overflow-hidden');
 		}
 	}
+
+	const menuItems = [
+		{ href: "/#section-reservation", text: "Réservation" },
+		{ href: "/menu", text: "Menu" },
+		{ href: "/#section-galerie", text: "Galerie" },
+		{ href: "/#section-promotions", text: "Promotions" },
+		{ href: "/#section-contact", text: "Contact" },
+	];
 </script>
 
 <nav
-	class="h-32 w-full fixed top-0 px-4 pt-4 bg-gold-gradient z-50 flex items-center justify-center md:justify-between drop-shadow-lg"
+	class="h-32 w-full fixed top-0 px-4 pt-4 bg-gold-gradient backdrop-blur-sm z-50 flex items-center justify-center md:justify-between drop-shadow-lg"
 >
 	<div class="h-full">
 		<a href="/"
-			><img
-				class="object-contain h-full drop-shadow-xl"
-				src="./img/100-genies-logo.png"
-				alt=""
-			/></a
+		><img
+			class="object-contain h-full drop-shadow-xl"
+			src="./img/100-genies-logo.png"
+			alt=""
+		/></a
 		>
 	</div>
 	<button class="md:hidden absolute top-2 right-2" on:click={toggleMenu}>
@@ -33,21 +41,11 @@
 	</button>
 	<div>
 		<ul class="hidden md:flex items-center font-bold space-x-8 tracking-wider pb-4 uppercase">
-			<li class="hover:border-b hover:shadow-lg drop-shadow-lg transition-all">
-				<a href="/#section-reservation">Réservation</a>
-			</li>
-			<li class="hover:border-b hover:shadow-lg drop-shadow-lg transition-all">
-				<a href="/menu">Menu</a>
-			</li>
-			<li class="hover:border-b hover:shadow-lg drop-shadow-lg transition-all">
-				<a href="/#section-galerie">Galerie</a>
-			</li>
-			<li class="hover:border-b hover:shadow-lg drop-shadow-lg transition-all">
-				<a href="/#section-promotions">Promotions</a>
-			</li>
-			<li class="hover:border-b hover:shadow-lg drop-shadow-lg transition-all">
-				<a href="/#section-contact">Contact</a>
-			</li>
+			{#each menuItems as item}
+				<li class="hover:border-b hover:shadow-lg drop-shadow-lg transition-all">
+					<a href={item.href}>{item.text}</a>
+				</li>
+			{/each}
 		</ul>
 	</div>
 </nav>
@@ -61,10 +59,10 @@
 	<ul
 		class="flex flex-col items-center justify-center font-bold space-y-8 tracking-wider h-full text-3xl uppercase"
 	>
-		<li class="drop-shadow-lg"><a on:click={toggleMenu} href="/#section-reservation">Réservation</a></li>
-		<li class="drop-shadow-lg"><a on:click={toggleMenu} href="/menu">Menu</a></li>
-		<li class="drop-shadow-lg"><a on:click={toggleMenu} href="/#section-galerie">Galerie</a></li>
-		<li class="drop-shadow-lg"><a on:click={toggleMenu} href="/#section-promotions">Promotions</a></li>
-		<li class="drop-shadow-lg"><a on:click={toggleMenu} href="/#section-contact">Contact</a></li>
+		{#each menuItems as item}
+			<li class="drop-shadow-lg">
+				<a on:click={toggleMenu} href={item.href}>{item.text}</a>
+			</li>
+		{/each}
 	</ul>
 </div>
